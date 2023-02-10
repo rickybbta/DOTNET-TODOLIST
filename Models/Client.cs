e9d7a9e01edfa647d23645a1755d7bd5a4f5a011
Ricardo <116693915+rickybbta@users.noreply.github.com>
2023-02-10 16:19:20 -0300

    att

---
+++ Models/Client.cs
@@ -0,0 +1,8 @@
+namespace TWTodolist.Models;
+
+public class Client{
+    public string nome { get; set; } = string.Empty;
+    public string email { get; set; }= string.Empty;
+    public uint compras { get; set; }
+    public uint tipo { get; set; }
+ }
---
+++ ViewModels/CardData.cs
@@ -0,0 +1,6 @@
+namespace TWTodolist.ViewModels;
+public class CardData{
+    public string title { get; set; } = string.Empty;
+    public string subtitle { get; set; } = string.Empty;
+    public string content { get; set; } = string.Empty;
+ }
